2134cca4a979aaa1ef68b49d05e711ec47c4777a
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-28 22:39:45 +0100

    Implemented quicksort algorithm.

---
+++ src/com/sparta/sorts/QuickSort.java
@@ -0,0 +1,66 @@
+package com.sparta.sorts;
+
+public class QuickSort implements SuperSorter, QuickSortInterface {
+
+    @Override
+    public void quickSort(int[] numbers, int left, int right) {
+        int i = left, j = right;
+        int tmp;
+        int pivot = numbers[(left + right) / 2];
+        /* partition */
+        while (i <= j)
+        {
+            while (numbers[i] < pivot)
+                i++;
+            while (numbers[j] > pivot)
+                j--;
+            if (i <= j)
+            {
+                tmp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+
+        /* recursion */
+        if (left < j)
+            quickSort(numbers, left, j);
+
+        if (i < right)
+            quickSort(numbers, i, right);
+    }
+
+    @Override
+    public void sort(int[] numbers) {
+
+        int i = 0, j = (numbers.length-1);
+        int tmp;
+        int pivot = numbers[(0 + (numbers.length)) / 2];
+        /* partition */
+        while (i <= j)
+        {
+            while (numbers[i] < pivot)
+                i++;
+            while (numbers[j] > pivot)
+                j--;
+            if (i <= j)
+            {
+                tmp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = tmp;
+                i++;
+                j--;
+            }
+        };
+
+        /* recursion */
+        if (0 < j)
+            quickSort(numbers, 0, j);
+
+        if (i < numbers.length-1)
+            quickSort(numbers, i, numbers.length-1);
+
+    }
+}
