e48890f3e1b7a8180da620f5e7f51b628d3f93f0
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-29 12:26:31 +0100

    Made some unexpected controller changes, created some view classes.

--- src/com/sparta/controller/TheArray.java
+++ src/com/sparta/controller/TheArray.java
@@ -5,7 +5,15 @@ package com.sparta.controller;
 public class TheArray implements ArrayMaker{
 
     private int arrayNum;
-    private int thisArray;
+    private int[] thisArray;
+
+    public int[] getThisArray() {
+        return thisArray;
+    }
+
+    public void setThisArray(int[] thisArray) {
+        this.thisArray = thisArray;
+    }
 
     public int getArrayNum() {
         return arrayNum;
@@ -15,22 +23,15 @@ public class TheArray implements ArrayMaker{
         this.arrayNum = arrayNum;
     }
 
-    public int getThisArray() {
-        return thisArray;
-    }
-
-    public void setThisArray(int thisArray) {
-        this.thisArray = thisArray;
-    }
 
     @Override
-    public int[] arrayMade() {
+    public void arrayMade() {
 
         int[] theArray = new int[this.arrayNum];
         for(int i = 0; i <  theArray.length; i++) {
             theArray[i] = (int)(Math.random() * 100);
         }
 
-        return theArray;
+        setThisArray(theArray);
     }
 }
---
+++ src/com/sparta/theviews/Displayer.java
@@ -0,0 +1,16 @@
+package com.sparta.theviews;
+
+import java.util.Scanner;
+
+public class Displayer {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter sort type b, m, q: ");
+
+
+    }
+
+}
