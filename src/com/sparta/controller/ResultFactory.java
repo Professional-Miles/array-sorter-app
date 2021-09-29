de970436e59005e4256ee9af9f70f609b40f43e9
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-29 15:01:28 +0100

    Implemented classes to collate results for user.

--- src/com/sparta/controller/ResultFactory.java
+++ src/com/sparta/controller/ResultFactory.java
@@ -5,9 +5,51 @@ import com.sparta.model.UserResults;
 
 public class ResultFactory {
 
+
     UserInput input = UserInput.getInstance();
     UserResults result = UserResults.getInstance();
+    private int[] tempArray;
 
+    public int[] getTempArray() {
+        return tempArray;
+    }
+
+    public void setTempArray(int[] tempArray) {
+        this.tempArray = tempArray;
+    }
+
+
+
+    public void setTypeResult(){
+        switch (input.getSortType()){
+            case "b":
+                result.setSortType("Bubble Sort");
+                break;
+            case "m":
+                result.setSortType("Merge Sort");
+                break;
+            case "q":
+                result.setSortType("Quick Sort");
+                break;
+        }
+    }
+
+    public void sizeResult(){
+        result.setArraySize(input.getArraySize());
+    }
+
+    public void arrayOriginalResult(){
+        TheArray arrayTemp = new TheArray();
+        arrayTemp.setArrayNum(input.getArraySize());
+        arrayTemp.arrayMade();
+        tempArray = arrayTemp.getThisArray();
+        result.setOriginalArray(tempArray);
+    }
+
+    public void arraySorterResult(){
+        SortManager sorter = new SortManager();
+        result.setSortedArray(sorter.arraySorter(tempArray, input.getSortType()));
+    }
 
 
 }
--- src/com/sparta/view/DisplayManager.java
+++ src/com/sparta/view/DisplayManager.java
@@ -16,6 +16,7 @@ public class DisplayManager {
         System.out.print("Enter sort type b, m, q: ");
         input.setSortType(scanner.nextLine());
 
+
         System.out.print("Enter number of ints to sort: ");
         input.setArraySize(scanner.nextInt());
 
