6d0fd570b92c1522c8dda14de3bdb84f0abb6fa6
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-29 12:56:13 +0100

    Added user input and data storage.

--- src/com/sparta/theviews/Display.java
+++ src/com/sparta/theviews/Display.java
@@ -2,14 +2,18 @@ package com.sparta.theviews;
 
 import java.util.Scanner;
 
-public class Displayer {
+public class Display {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        UserInput input = new UserInput();
 
         System.out.print("Enter sort type b, m, q: ");
+        input.setSortType(scanner.nextLine());
 
+        System.out.print("Enter number of ints to sort: ");
+        input.setArraySize(scanner.nextInt());
 
     }
 
