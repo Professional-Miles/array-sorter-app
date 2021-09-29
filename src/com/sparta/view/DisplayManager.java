c6ab1738fee98a052fe152b3b65f736ab6a2bc7f
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-29 15:29:34 +0100

    Implemenation works but original array record is replaced by the sorted one. Working on a fix.

--- src/com/sparta/view/DisplayManager.java
+++ src/com/sparta/view/DisplayManager.java
@@ -1,5 +1,6 @@
 package com.sparta.view;
 
+import com.sparta.controller.ResultFactory;
 import com.sparta.model.UserInput;
 import com.sparta.model.UserResults;
 
@@ -11,15 +12,21 @@ public class DisplayManager {
 
         Scanner scanner = new Scanner(System.in);
         UserInput input = UserInput.getInstance();
+        ResultFactory factory = new ResultFactory();
         UserResults result = UserResults.getInstance();
 
         System.out.print("Enter sort type b, m, q: ");
         input.setSortType(scanner.nextLine());
 
-
         System.out.print("Enter number of ints to sort: ");
         input.setArraySize(scanner.nextInt());
 
+        factory.setTypeResult();
+        factory.sizeResult();
+        factory.arrayOriginalResult();
+        factory.arraySorterResult();
+
+        System.out.println(result.toString());
 
 
 
