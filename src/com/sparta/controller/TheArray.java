9d6e9bf4245bb24b9cacccdf997a650b42fa6a83
Professional-Miles <90847818+Professional-Miles@users.noreply.github.com>
2021-09-29 08:52:25 +0100

    Refactor of object attribute.

--- src/com/sparta/controller/TheArray.java
+++ src/com/sparta/controller/TheArray.java
@@ -4,22 +4,22 @@ import java.util.Arrays;
 
 public class TheArray implements ArrayMaker{
 
-    private int arraynum;
+    private int arrayNum;
 
     @Override
     public void setArrayNumber(int number) {
-        this.arraynum = number;
+        this.arrayNum = number;
     }
 
     @Override
     public int getArrayNumber() {
-        return this.arraynum;
+        return this.arrayNum;
     }
 
     @Override
     public int[] arrayMade() {
 
-        int[] theArray = new int[this.arraynum];
+        int[] theArray = new int[this.arrayNum];
         for(int i = 0; i <  theArray.length; i++) {
             theArray[i] = (int)(Math.random() * 100);
         }
